fe857369591e0e4751c2b45f4003d1d45b3bd8a2
David Goldfarb <deg@degel.com>
2013-04-19 11:57:35 +0300

    Tutorial 11: Client-side basic validation.

--- client-src/degel/receipts/client.cljs
+++ client-src/degel/receipts/client.cljs
@@ -2,13 +2,13 @@
   (:require-macros [hiccups.core :refer [html]])
   (:require [domina :refer [append! by-class by-id destroy! set-value! value]]
             [hiccups.runtime] ;; Needed by hiccups.core macros
-            [domina.events :refer [listen!]]
+            [domina.events :refer [listen! prevent-default]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]]
             [cljs.reader :refer [read-string]]))
 
 
 ;; define the function to be attached to form submission event
-(defn validate-form []
+(defn submit-form [event]
   (let [paid-by (value (by-id "PaidBy"))
         date (value (by-id "Date"))
         amount (value (by-id "Amount"))
@@ -16,15 +16,18 @@
         vendor (value (by-id "Vendor"))
         comments (value (by-id "Comments"))
         for-whom (value (by-id "ForWhom"))]
-    (set-value! (by-id "ForWhom") "DASH")
-    (remote-callback :save-receipt [paid-by date amount category vendor comments for-whom]
-                     #(append! (by-id "newReceipt")
-                                   (html [:div.result %])))))
+    (if (or (empty? paid-by) (empty? date) (empty? category) (empty? vendor))
+      (do (prevent-default event)
+          (js/alert "Missing field"))
+      (remote-callback :save-receipt [paid-by date amount category vendor comments for-whom]
+                       #(append! (by-id "newReceipt")
+                                 (html [:div.result %])))))
+)
 
 
 (defn add-help []
   (append! (by-id "newReceipt")
-           (html [:div.help "Help me please"])))
+           (html [:div.help "Click here to submit receipt"])))
 
 
 (defn remove-help []
@@ -34,7 +37,7 @@
 (defn ^:export init []
   (when (and js/document
              (aget js/document "getElementById"))
-    (listen! (by-id "submit") :click validate-form)
+    (listen! (by-id "submit") :click submit-form)
     (listen! (by-id "submit") :mouseover add-help)
     (listen! (by-id "submit") :mouseout remove-help)
     (listen! (by-id "Date") :mouseover add-help)
