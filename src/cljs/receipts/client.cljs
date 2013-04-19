d55eeffa95b3316f40424ac2ccb3568393761055
David Goldfarb <deg@degel.com>
2013-04-19 10:06:52 +0300

    Use namespaces in :refer style.

--- src/cljs/receipts/client.cljs
+++ src/cljs/receipts/client.cljs
@@ -1,41 +1,41 @@
 (ns degel.receipts.cljs.client
-  (:require-macros [hiccups.core :as h])
-  (:require [domina :as dom]
-            [hiccups.runtime :as hiccupsrt]
-            [domina.events :as ev]
+  (:require-macros [hiccups.core :refer [html]])
+  (:require [domina :refer [append! by-class by-id destroy! set-value! value]]
+            [hiccups.runtime] ;; Needed by hiccups.core macros
+            [domina.events :refer [listen!]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]]
             [cljs.reader :refer [read-string]]))
 
+
 ;; define the function to be attached to form submission event
 (defn validate-form []
-  (let [paid-by (dom/value (dom/by-id "PaidBy"))
-        date (dom/value (dom/by-id "Date"))
-        amount (dom/value (dom/by-id "Amount"))
-        category (dom/value (dom/by-id "Category"))
-        vendor (dom/value (dom/by-id "Vendor"))
-        comments (dom/value (dom/by-id "Comments"))
-        for-whom (dom/value (dom/by-id "ForWhom"))]
-    (dom/set-value! (dom/by-id "ForWhom") "DASH")
+  (let [paid-by (value (by-id "PaidBy"))
+        date (value (by-id "Date"))
+        amount (value (by-id "Amount"))
+        category (value (by-id "Category"))
+        vendor (value (by-id "Vendor"))
+        comments (value (by-id "Comments"))
+        for-whom (value (by-id "ForWhom"))]
+    (set-value! (by-id "ForWhom") "DASH")
     (remote-callback :save-receipt [paid-by date amount category vendor comments for-whom]
-                     #(dom/append! (dom/by-id "newReceipt")
-                                   (h/html [:div.result %])))))
+                     #(append! (by-id "newReceipt")
+                                   (html [:div.result %])))))
+
 
 (defn add-help []
-  (dom/append! (dom/by-id "newReceipt")
-               (h/html [:div.help "Help me please"])))
+  (append! (by-id "newReceipt")
+           (html [:div.help "Help me please"])))
+
 
 (defn remove-help []
-  (dom/destroy! (dom/by-class "help")))
+  (destroy! (by-class "help")))
 
-;; define the function to attach validate-form to onsubmit event of
-;; the form
+
 (defn ^:export init []
-  ;; verify that js/document exists and that it has a getElementById
-  ;; property
   (when (and js/document
              (aget js/document "getElementById"))
-    (ev/listen! (dom/by-id "submit") :click validate-form)
-    (ev/listen! (dom/by-id "submit") :mouseover add-help)
-    (ev/listen! (dom/by-id "submit") :mouseout remove-help)
-    (ev/listen! (dom/by-id "Date") :mouseover add-help)
-    (ev/listen! (dom/by-id "Date") :mouseout remove-help)))
+    (listen! (by-id "submit") :click validate-form)
+    (listen! (by-id "submit") :mouseover add-help)
+    (listen! (by-id "submit") :mouseout remove-help)
+    (listen! (by-id "Date") :mouseover add-help)
+    (listen! (by-id "Date") :mouseout remove-help)))
