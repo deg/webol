e0958da2415dde48078466395d9f797a50dba587
David Goldfarb <deg@degel.com>
2013-04-19 17:54:16 +0300

    Tutorial 12: Get info from HTML attributes.

--- client-src/degel/receipts/client.cljs
+++ client-src/degel/receipts/client.cljs
@@ -1,6 +1,6 @@
 (ns degel.receipts.client
   (:require-macros [hiccups.core :refer [html]])
-  (:require [domina :refer [append! by-class by-id destroy! set-value! value]]
+  (:require [domina :refer [append! attr by-class by-id destroy! set-value! value]]
             [hiccups.runtime] ;; Needed by hiccups.core macros
             [domina.events :refer [listen! prevent-default target]]
             [shoreleave.remotes.http-rpc :refer [remote-callback]]
@@ -24,9 +24,10 @@
                                  (html [:div.result %]))))))
 
 (defn verify-not-empty [e]
-  (let [target (target e)]
+  (let [target (target e)
+        message (attr target :title)]
     (when (empty? (value e))
-      (append! (by-id "newReceipt") (html [:div.help "Empty, tsk tsk!"])))))
+      (append! (by-id "newReceipt") (html [:div.help message])))))
 
 
 (defn add-help []
@@ -43,11 +44,13 @@
              (aget js/document "getElementById"))
     (let [form (by-id "submit")
           paid-by (by-id "PaidBy")
+          amount (by-id "Amount")
           date (by-id "Date")]
       (listen! form :click submit-form)
       (listen! form :mouseover add-help)
       (listen! form :mouseout remove-help)
       (listen! paid-by :blur verify-not-empty)
       (listen! date :blur verify-not-empty)
+      (listen! amount :blur verify-not-empty)
       (listen! date :mouseover add-help)
       (listen! date :mouseout remove-help))))
