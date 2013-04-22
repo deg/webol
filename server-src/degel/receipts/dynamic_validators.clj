324692c3ad8b74d3efa4a9982f694508b5d3314b
David Goldfarb <deg@degel.com>
2013-04-22 19:06:59 +0300

    Remove dead code.

--- server-src/degel/receipts/dynamic_validators.clj
+++ server-src/degel/receipts/dynamic_validators.clj
@@ -2,19 +2,4 @@
   (:require [valip.core :refer [validate]]
             [valip.predicates :refer [present? matches email-address?]]))
 
-(defn notUsed-validate-receipt-fields [paid-by date amount category vendor comments for-whom]
-  (validate
-   {:paid-by paid-by
-    :date date
-    :amount amount
-    :category category
-    :vendor vendor
-    :comments comments
-    :for-whom for-whom}
-   [:paid-by present? "paid-by can't be empty."]
-   [:date present? "date can't be empty."]
-   [:amount present? "amount can't be empty."]
-   [:category present? "category can't be empty."]
-   [:vendor present? "vendor can't be empty."]
-   [:comments present? "comments can't be empty."]
-   [:for-whom present? "for-whom can't be empty."]))
+
