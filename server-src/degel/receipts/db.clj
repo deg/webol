e85f48e20a0a8684031b0961608328bae66d5c72
David Goldfarb <deg@degel.com>
2013-04-22 17:46:22 +0300

    Use AWS SimpleDB, via Rummage, to store receipts persistently.

---
+++ server-src/degel/receipts/db.clj
@@ -0,0 +1,43 @@
+(ns degel.receipts.db
+  (:require [cemerick.rummage :as sdb]
+            [cemerick.rummage.encoding :as enc]))
+(def aws-key "AKIAI2SJMRRD53FHKY3Q")
+(defn assemble-aws-secret [secret]
+  ;; Reminder: (MAYNARD (ROOT 1654)) or https://portal.aws.amazon.com/gp/aws/securityCredentials
+  (str "PqoUYiVHA" (get secret 0)
+       "pRhV00HZ7YZ" (get secret 1)
+       "Zc7ZF" (get secret 2)
+       "3JCt" (get secret 3)
+       "ECEnPdc"))
+
+
+(def the-client (atom nil))
+(def the-config (atom nil))
+(defn create-client [secret]
+  (reset! the-client (sdb/create-client aws-key (assemble-aws-secret secret)))
+  (reset! the-config (assoc enc/all-strings :client @the-client)))
+
+
+(defn put-record [& {:keys [paid-by date currency amount category vendor comments for-whom] :as columns}]
+  (let [guid (str (java.util.UUID/randomUUID))]
+    (sdb/put-attrs @the-config "Receipts" (assoc columns ::sdb/id guid))
+    guid))
+
+
+(defn nuke-db [secret]
+  (create-client secret)
+  (sdb/delete-domain @the-client "Receipts")
+  (sdb/create-domain @the-client "Receipts"))
+
+(defn test-db [secret]
+  (nuke-db secret)
+  (put-record :paid-by "Visa" :date "2013-04-13"
+              :currency "NIS" :amount "5.46"
+              :category "Food" :Vendor "Shufersal" :comments "Shufersal online"
+              :for-whom "DASH")
+  (put-record :paid-by "Check" :Check# "1234" :date "2013-04-28"
+              :currency "USD" :amount "15.23"
+              :category "Books" :Vendor "Barnes & Noble" :comments "At airport"
+              :for-whom "D")
+  (map #(dissoc % :cemerick.rummage/id)
+       (sdb/query-all @the-config '{select * from Receipts})))
