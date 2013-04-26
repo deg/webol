6f674b9df1adfd24ddd75c0332a677fa13650bcc
David Goldfarb <deg@degel.com>
2013-04-26 11:38:39 +0300

    Cleanup project def for library collisions and deployment-readiness.

--- project.clj
+++ project.clj
@@ -2,15 +2,20 @@
   :description "HTML5 web-app to manage our receipts."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
+
+                 ;; Degel's Clojure utility library
+                 [degel-clojure-utils "0.1.2"  :exclusions [org.clojure/tools.macro]]
+
+                 [compojure "1.1.5" :exclusions [ring/ring-core]]
                  [domina "1.0.2-SNAPSHOT"]
                  [hiccups "0.2.0"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
                  [shoreleave/shoreleave-remote "0.3.0"]
                  [com.cemerick/valip "0.3.2"]
-                 [com.cemerick/rummage "1.0.1"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-ring "0.8.3"]]
+                 [com.cemerick/rummage "1.0.1" :exclusions [commons-codec]]]
+  :plugins [[lein-cljsbuild "0.3.0" :exclusions [org.clojure/clojure]]
+            [lein-ring "0.8.3" :exclusions [org.clojure/clojure]]
+            [lein-pedantic "0.0.5"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["server-src"]
   :ring {:handler degel.receipts.remotes/app}
@@ -22,8 +27,8 @@
             :compiler {:output-to "resources/public/js/receipts-dev.js"
                        :optimizations :whitespace
                        :pretty-print true}
-            :jar true}
-      :prod {
+            :jar false}
+      :production {
             :source-paths ["client-src" "client-src-dev"] ; TODO brepl not safe here
             :compiler {:output-to "resources/public/js/receipts.js"
                        :optimizations :advanced
