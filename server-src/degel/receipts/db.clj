5133648ffd06315f082eb00b3cd7c474c3430c67
David Goldfarb <deg@degel.com>
2013-05-07 12:25:25 +0300

    Cleanup db interface and add get-all-records function.

--- server-src/degel/receipts/db.clj
+++ server-src/degel/receipts/db.clj
@@ -2,45 +2,72 @@
   (:require [cemerick.rummage :as sdb]
             [cemerick.rummage.encoding :as enc]))
 (def aws-key "AKIAI2SJMRRD53FHKY3Q")
-(defn assemble-aws-secret [pwd]
+(defn assemble-aws-secret [password]
   ;; Reminder: (MAYNARD (ROOT 1654)) or https://portal.aws.amazon.com/gp/aws/securityCredentials
-  (str "PqoUYiVHA" (get pwd 0)
-       "pRhV00HZ7YZ" (get pwd 1)
-       "Zc7ZF" (get pwd 2)
-       "3JCt" (get pwd 3)
+  (str "PqoUYiVHA" (get password 0)
+       "pRhV00HZ7YZ" (get password 1)
+       "Zc7ZF" (get password 2)
+       "3JCt" (get password 3)
        "ECEnPdc"))
 
 
 (def the-client (atom nil))
 (def the-config (atom nil))
-(defn create-client [pwd]
-  (reset! the-client (sdb/create-client aws-key (assemble-aws-secret pwd)))
-  (reset! the-config (assoc enc/all-strings :client @the-client)))
+(defn create-client
+  "Connect to DB and validate the password. Return [success error-message]"
+  [password]
+  (if (nil? @the-config)
+    (let [client (sdb/create-client aws-key (assemble-aws-secret password))
+          config (assoc enc/keyword-strings :client client)]
+      ;; I think this is the cheapest operation that will test the password
+      (try (do (sdb/query config '{select count from Receipts})
+               (reset! the-client client)
+               (reset! the-config config)
+               [true "New connection"])
+           (catch com.amazonaws.AmazonServiceException e
+             [false (str "DB connection failed. Status code: "
+                         (.getStatusCode e) ": "
+                         (.getMessage e))])))
+    [true "existing connection"]))
 
 
 (defn put-record [columns]
-  (when (nil? @the-config)
-    (create-client (:password columns)))
-  (let [guid (str (java.util.UUID/randomUUID))]
-    (sdb/put-attrs @the-config "Receipts"
-                   (assoc (dissoc columns :password) ::sdb/id guid))
-    guid))
+  (let [[success errmsg] (create-client (:password columns))]
+    (if (false? success)
+      errmsg
+      (let [guid (str (java.util.UUID/randomUUID))]
+        (sdb/put-attrs @the-config "Receipts"
+                       (assoc (dissoc columns :password) ::sdb/id guid))
+        guid))))
+
+(defn get-all-records [password columns]
+  (let [[success errmsg] (create-client password)]
+    (if (false? success)
+      errmsg
+      (map #(dissoc % ::sdb/id) (sdb/query-all @the-config `{select ~columns from Receipts})))))
 
 
-(defn nuke-db [pwd]
-  (create-client pwd)
-  (sdb/delete-domain @the-client "Receipts")
-  (sdb/create-domain @the-client "Receipts"))
+(defn nuke-db [password]
+  (let [[success errmsg] (create-client password)]
+    (if (false? success)
+      errmsg
+      (do
+        (sdb/delete-domain @the-client "Receipts")
+        (sdb/create-domain @the-client "Receipts")))))
 
-(defn test-db [pwd]
-  (nuke-db pwd)
-  (put-record :paid-by "Visa" :date "2013-04-13"
-              :currency "NIS" :amount "5.46"
-              :category "Food" :Vendor "Shufersal" :comments "Shufersal online"
-              :for-whom "DASH")
-  (put-record :paid-by "Check" :Check# "1234" :date "2013-04-28"
-              :currency "USD" :amount "15.23"
-              :category "Books" :Vendor "Barnes & Noble" :comments "At airport"
-              :for-whom "D")
+(defn test-db [password]
+  (nuke-db password)
+  (put-record {:paid-by "Visa" :date "2013-04-13"
+               :currency "NIS" :amount "5.46"
+               :category "Food" :Vendor "Shufersal" :comments "Shufersal online"
+               :for-whom "DASH"
+               :formatted "Dummy"
+               :password password})
+  (put-record {:paid-by "Check" :Check# "1234" :date "2013-04-28"
+               :currency "USD" :amount "15.23"
+               :category "Books" :Vendor "Barnes & Noble" :comments "At airport"
+               :for-whom "D"
+               :formatted "Dummy" ;; [TODO] Generate this at right level
+               :password password})
   (map #(dissoc % :cemerick.rummage/id)
        (sdb/query-all @the-config '{select * from Receipts})))
