00d84a618be1671cf772b6ce0e723877e9fd1403
David Goldfarb <deg@degel.com>
2013-05-20 20:06:11 +0300

    Combine remotes.clj into server.clj, otherwise remotes did not work. (It looks like wrap-rpc needs to be in the same file as the defremote's, though I don't understand why).

--- server-src/degel/receipts/server.clj
+++ server-src/degel/receipts/server.clj
@@ -4,8 +4,20 @@
             [compojure.route :refer [resources not-found]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.util.response :refer [redirect]]
-            [shoreleave.middleware.rpc :refer [wrap-rpc]]
-            [compojure.handler :refer [site]]))
+            [shoreleave.middleware.rpc :refer [wrap-rpc defremote]]
+            [compojure.handler :refer [site]]
+            [degel.receipts.receipts :refer [collect-receipt-history enter-receipt-internal]]))
+
+
+
+(defremote fill-paid-by [country]
+  ["Cash" "Ck #" "other" "v0223" "v5760" "v9949" "HowdyDo"])
+
+(defremote fill-receipt-history [password]
+  (remove nil? (collect-receipt-history password)))
+
+(defremote enter-receipt [columns]
+  (enter-receipt-internal columns))
 
 
 (defroutes app-routes
