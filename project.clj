d9bf5b0090004408f313fa3914a529e9ef6d23f7
David Goldfarb <deg@degel.com>
2013-05-21 13:53:14 +0300

    Cleanups to project.clj, mostly for lein-pendantic.

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
 
                  ;; Degel's Clojure utility library
-                 [degel-clojure-utils "0.1.2"  :exclusions [org.clojure/tools.macro]]
+                 [degel-clojure-utils "0.1.2"]
 
                  ;; Needed, I think, in order to build a runnable uberjar with compojure
                  [ring/ring-jetty-adapter "1.1.8"]
 
-                 [compojure "1.1.5" #_:exclusions #_[ring/ring-core]]
+                 [compojure "1.1.5" :exclusions [ring/ring-core org.clojure/tools.macro]]
                  [domina "1.0.2-SNAPSHOT"]
                  [hiccups "0.2.0"]
                  [shoreleave/shoreleave-remote-ring "0.3.0"]
@@ -20,20 +20,16 @@
                  [com.cemerick/rummage "1.0.1" :exclusions [commons-codec]]]
   :plugins [[lein-cljsbuild "0.3.2" :exclusions [org.clojure/clojure]]
             [lein-ring "0.8.3" :exclusions [org.clojure/clojure]]
-            ;; [TODO] Fix. For now, no pedantic, because of conflicts in compojure dependency
-            ;; on ring/ring.core in local vs. Heroku deployment.
-            #_[lein-pedantic "0.0.5"]]
+            [lein-pedantic "0.0.5"]]
   :min-lein-version "2.0.0"
   :source-paths ["server-src"]
   :ring {:handler degel.receipts.remotes/app}
+  ;:profiles {:dev {:hooks [leiningen.cljsbuild]}}
   :main degel.receipts.server
   :cljsbuild {
     :crossovers [valip.core valip.predicates degel.receipts.static-validators]
     :builds {
       :dev {
-            ;; [TODO] Figure out why having this at top-level breaks
-            ;; 'lein trampoline ring server' and even 'lein trampoline run'
-            :hooks [leiningen.cljsbuild]
             :source-paths ["client-src" "client-src-dev"]
             :compiler {:output-to "resources/public/js/receipts-dev.js"
                        :optimizations :whitespace
