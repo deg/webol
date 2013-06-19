123e474454f7091e478fa1f98d00c29c78846fd3
David Goldfarb <deg@degel.com>
2013-06-19 15:39:30 +0300

    Remove more table names from generic code.

--- server-src/degel/receipts/server.clj
+++ server-src/degel/receipts/server.clj
@@ -7,7 +7,7 @@
             [shoreleave.middleware.rpc :refer [wrap-rpc defremote]]
             [compojure.handler :refer [site]]
             [degel.cljutil.devutils :as dev]
-            [degel.receipts.simpleDB :refer [put-record get-record]]
+            [degel.receipts.simpleDB :refer [put-record get-record nuke-db]]
             [degel.receipts.receipts :refer [collect-receipt-history enter-receipt-internal]]))
 
 
@@ -27,6 +27,12 @@
                  :uid (str [user-id key])
                  :value value}]
     (put-record "User-data" columns)))
+
+(defn init-db [password]
+  (nuke-db password)
+  (write-storage :PaidBy-options (str ["Cash" "Ck (# in comment)" "v0223" "v5760" "v9949" "Other"])
+                 nil
+                 password))
 
 
 (defremote read-storage [key user-id password]
