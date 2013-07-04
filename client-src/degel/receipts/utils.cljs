308ec1299ea6a9f3460d96d635dbd0acccbc360d
David Goldfarb <deg@degel.com>
2013-07-04 14:43:40 +0300

    Add safe-read-string util.

--- client-src/degel/receipts/utils.cljs
+++ client-src/degel/receipts/utils.cljs
@@ -1,4 +1,7 @@
-(ns degel.receipts.utils)
+(ns degel.receipts.utils
+  (:require
+   ;; [TODO] Is clojure.edn available in cljs?
+   [cljs.reader :refer [read-string]]))
 
 (defn now-string []
   (let [date (js/Date.)
@@ -7,3 +10,10 @@
     (str (.getFullYear date) "-"
          (if (< month 10) "0" "") month "-"
          (if (< day 10) "0" "") day)))
+
+(defn safe-read-string
+  "Read a string or nil, or return clojure value."
+  [str]
+  (if (empty? str)
+    nil
+    (read-string str)))
