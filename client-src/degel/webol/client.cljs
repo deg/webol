1d194ddc9b2078f8a174d03b5cd841cb28be4346
David Goldfarb <deg@degel.com>
2013-07-31 10:23:17 +0300

    Webol basic page layout.

--- client-src/degel/webol/client.cljs
+++ client-src/degel/webol/client.cljs
@@ -1,7 +1,31 @@
-(ns degel.webol.client)
+(ns degel.webol.client
+  (:require [domina :as dom :refer [log]]
+            [redmapel :as rml]
+            [degel.webol.page :as page]))
 
 
+(def ^:export webol-tree (rml/make-redmapel-tree))
+
+(def memory-per-row 10)
+(def memory-num-rows 10)
+
+
+(defn clear-all []
+  (doseq [n (range (* memory-per-row memory-num-rows))]
+    (rml/put! webol-tree [:memory n] 0))
+  (rml/put! webol-tree [:register :pc] 0)
+  (rml/put! webol-tree [:register :sp] nil))
+
+
+(defn render-screen []
+  (let [canvas (dom/by-id "canvas1")
+        ctx    (.getContext canvas "2d")
+        width  (.-width canvas)
+        height (.-height canvas)]
+    (set! (-> canvas .-style .-border) "2px dashed gray")
+    [canvas ctx width height]))
 
 
 (defn ^:export init []
-)
+  (dom/set-html! (dom/by-id "page") (page/webol-page))
+  (dom/set-html! (dom/by-id "memory") (page/table memory-num-rows memory-per-row)))
---
+++ client-src/degel/webol/page.cljs
@@ -0,0 +1,27 @@
+(ns degel.webol.page
+  (:require-macros [hiccups.core :refer [html]])
+  (:require [hiccups.runtime] ;; Needed by hiccups.core macros
+            [domina :as dom :refer [log]]))
+
+
+(defn table [rows columns]
+  (html
+   [:table
+    (doall
+     (map (fn [r]
+            [:tr (map (fn [c]
+                        [:td (str "R:" r ";C:" c)])
+                      (range columns))])
+          (range rows)))]))
+
+(defn webol-page []
+  (html
+   [:div#main
+    [:h1 "Webol Computer"]
+    [:h2 "Copyright (C) 2013; " [:a {:href "mailto:deg@degel.com"} "David Goldfarb"]]
+    [:canvas#sketchboard
+     {:width 500 :height 300 :style "border: 2px solid blue"}
+     "This browser does not support canvases"]
+    [:table#memory]
+    [:table#registers]
+    [:div#program]]))
