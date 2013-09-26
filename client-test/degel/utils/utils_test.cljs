4675870122e380fa99c03d7dd2f68b2581815dc4
David Goldfarb <deg@degel.com>
2013-09-26 22:48:37 +0300

    First real cljs tests.

--- client-test/degel/utils/utils_test.cljs
+++ client-test/degel/utils/utils_test.cljs
@@ -1,8 +1,10 @@
 (ns degel.utils.utils-test
   (:require-macros [cemerick.cljs.test :refer (is deftest with-test run-tests testing)])
-  (:require [cemerick.cljs.test :as t]))
+  (:require [cemerick.cljs.test :as t]
+            [degel.utils.utils :as utils]))
 
-(deftest dummy
-  (is (= 1 1))
-  (is (= 1 2)))
-
+(deftest test-read-string-or-nil
+  (is (= (utils/read-string-or-nil nil) nil))
+  (is (= (utils/read-string-or-nil "") nil))
+  (is (= (utils/read-string-or-nil "123") 123))
+  (is (= (utils/read-string-or-nil "(+ 1 2)") '(+ 1 2))))
