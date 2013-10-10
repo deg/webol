236a9ca7c044a5c9b599478803c5de93c2f05ffb
David Goldfarb <deg@degel.com>
2013-10-10 14:23:30 +0300

    Move grammar inline into source file, to give us one less issue to worry about while deploying now.

--- server-src/degel/webol/parser.clj
+++ server-src/degel/webol/parser.clj
@@ -2,13 +2,91 @@
   (:require [clojure.edn]
             [instaparse.core :as insta]))
 
-(defn read-grammar
-  ([]
-     (read-grammar "../receipts/server-src/degel/webol/grammar.txt"))
-  ([filename]
-     (slurp filename)))
+;;; (defn read-grammar
+;;;  ([]
+;;;     (read-grammar "../receipts/server-src/degel/webol/grammar.txt"))
+;;;  ([filename]
+;;;     (slurp filename)))
 
-(def ^:private line-parser (insta/parser (read-grammar)))
+;; [TODO] 10Oct13 - Need to move grammar into some kind of resource file.
+;;        Loading from grammar.txt file was problematic because I couldn't
+;;        find a path that would work from other projects and Heroku.
+(def ^:private grammar
+  "
+(* A full line to be parsed *)
+input-line = <whitespace>? (action | (!action bad-cmd) | progline) <whitespace>?
+
+(* Immediate mode commands *)
+<action> = clear-cmd | edit-cmd | help-cmd | list-cmd | manual-cmd | println-cmd | print-cmd |
+           renumber-cmd | run-cmd | trace-cmd
+
+clear-cmd     = <#\"(?i)clear\">
+edit-cmd     = <#\"(?i)edit\"> <whitespace>? line-num
+help-cmd     = <#\"(?i)help\">
+list-cmd     = <#\"(?i)list\">
+manual-cmd   = <(#\"(?i)man\"|#\"(?i)manual\")> (<whitespace> expr)*
+println-cmd    = <#\"(?i)print\"> <whitespace>? expr-list
+print-cmd    = <#\"(?i)print\"> <whitespace>? expr-list <comma-delim>
+renumber-cmd = <#\"(?i)renumber\"> (<whitespace> line-num)?
+run-cmd      = <#\"(?i)run\"> (<whitespace> line-num)?
+trace-cmd    = <#\"(?i)trace\">
+
+bad-cmd      = #\"[a-zA-Z].*\"
+
+progline  = line-num <whitespace>? statement
+
+statement = dim-statement | goto-statement | if-statement | let-statement|
+            print-statement | rem-statement
+
+dim-statement = <#\"(?i)dim\"> <whitespace>? var-list
+goto-statement = <#\"(?i)goto\"> <whitespace>? line-num
+if-statement = <#\"(?i)if\"> <whitespace>? condition <whitespace>? <#\"(?i)then\"> <whitespace>? statement
+let-statement = <#\"(?i)let\">  <whitespace>? var <whitespace>? <\"=\"> <whitespace>? expr
+<print-statement> = print-cmd | println-cmd
+rem-statement = <#\"(?i)rem\"> comment
+
+condition = expr <whitespace>? cond-op <whitespace>? expr
+<cond-op> = \"==\" | \"!=\" | \"<\" | \"<=\" | \">\" | \">=\"
+
+<var-list> = var | var-list <comma-delim> var
+
+comment = #\".*\"
+
+<expr-list> = expr | expr-list <comma-delim> expr
+<expr> = quoted-string | arith
+
+<quoted-string> = <dquote> string-with-embedded-dquotes <dquote>
+<simple-string> = #\"[^\\\"]*\"
+string-with-embedded-dquotes = simple-string | simple-string '\"\"' string-with-embedded-dquotes
+
+<comma-delim> = <whitespace>? <\",\"> <whitespace>?
+
+line-num = integer
+
+(* Arithmetic expression parser *)
+<arith> = add-sub
+<add-sub> = mul-div | add | sub
+add = add-sub <'+'> mul-div
+sub = add-sub <'-'> mul-div
+<mul-div> = term | mul | div
+mul = mul-div <'*'> term
+div = mul-div <'/'> term
+<term> = atom | parens
+parens = <'('> add-sub <')'>
+<atom> = number | var
+var = #'[A-Za-z][A-Za-z0-9]*'
+<number> = integer | float
+float = #'[0-9]*\\.[0-9]+'
+integer = #'[0-9]+'
+
+(* Lexical items *)
+dquote = '\"'
+squote = \"'\"
+whitespace = #'\\s+'
+"
+  )
+
+(def ^:private line-parser (insta/parser grammar #_(read-grammar)))
 
 (defn- err-to-str [parse-result]
   (-> parse-result insta/get-failure
